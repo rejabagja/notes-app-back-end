48cc1ccfa6dd3cef763fccdbc055ef2c6caa907a
rejabagja <rezasubagja5@gmail.com>
2025-07-25 06:29:08 +0700

    feat(database): add notes table migration and add env variables

---
+++ migrations/1753188260951_create-table-notes.js
@@ -0,0 +1,52 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+// const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const up = (pgm) => {
+  pgm.createTable('notes', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    body: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    tags: {
+      type: 'TEXT[]',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+const down = (pgm) => {
+  pgm.dropTable('notes');
+};
+
+module.exports = { up, down };
