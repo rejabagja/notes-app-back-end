10d91b571b01394b6748a0672a148737f8c3c773
rejabagja <rezasubagja5@gmail.com>
2025-07-25 07:02:18 +0700

    feat(postgres): notes postgres database service implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -10,11 +10,11 @@ class NotesHandler {
     this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
   }
 
-  postNoteHandler(request, h) {
+  async postNoteHandler(request, h) {
     this._validator.validateNotePayload(request.payload);
     const { title = 'untitled', tags, body } = request.payload;
 
-    const noteId = this._service.addNote({ title, tags, body });
+    const noteId = await this._service.addNote({ title, tags, body });
 
     const response = h.response({
       status: 'success',
@@ -27,8 +27,8 @@ class NotesHandler {
     return response;
   }
 
-  getNotesHandler() {
-    const notes = this._service.getNotes();
+  async getNotesHandler() {
+    const notes = await this._service.getNotes();
     return {
       status: 'success',
       data: {
@@ -37,9 +37,9 @@ class NotesHandler {
     };
   }
 
-  getNoteByIdHandler(request) {
+  async getNoteByIdHandler(request) {
     const { id } = request.params;
-    const note = this._service.getNoteById(id);
+    const note = await this._service.getNoteById(id);
     return {
       status: 'success',
       data: {
@@ -48,12 +48,12 @@ class NotesHandler {
     };
   }
 
-  putNoteByIdHandler(request) {
+  async putNoteByIdHandler(request) {
     this._validator.validateNotePayload(request.payload);
     const { id } = request.params;
     const { title, tags, body } = request.payload;
 
-    this._service.editNoteById(id, { title, tags, body });
+    await this._service.editNoteById(id, { title, tags, body });
 
     return {
       status: 'success',
@@ -61,10 +61,10 @@ class NotesHandler {
     };
   }
 
-  deleteNoteByIdHandler(request) {
+  async deleteNoteByIdHandler(request) {
     const { id } = request.params;
 
-    this._service.deleteNoteById(id);
+    await this._service.deleteNoteById(id);
 
     return {
       status: 'success',
