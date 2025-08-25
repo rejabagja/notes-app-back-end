630814a93cd6bd632f04a6afa9c4e7e7ba172469
rejabagja <rezasubagja5@gmail.com>
2025-08-25 09:56:53 +0700

    local storage implemented

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const Inert = require('@hapi/inert');
+const path = require('path');
 const ClientError = require('./exceptions/ClientError');
 
 const notes = require('./api/notes');
@@ -25,12 +27,19 @@ const _exports = require('./api/exports');
 const ProducerService = require('./services/rabbitmq/ProducerSevice');
 const ExportsValidator = require('./validator/exports');
 
+const uploads = require('./api/uploads');
+const StorageService = require('./services/storage/StorageService');
+const UploadsValidator = require('./validator/uploads');
+
 
 const init = async () => {
   const collaborationsService = new CollaborationsService();
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  const storageService = new StorageService(
+    path.resolve(__dirname, 'api/uploads/file/images')
+  );
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -45,7 +54,10 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt
-    }
+    },
+    {
+      plugin: Inert
+    },
   ]);
 
   server.auth.strategy('notesapp_jwt', 'jwt', {
@@ -101,6 +113,13 @@ const init = async () => {
       options: {
         service: ProducerService,
         validator: ExportsValidator,
+      },
+    },
+    {
+      plugin: uploads,
+      options: {
+        service: storageService,
+        validator: UploadsValidator,
       },
     },
   ]);
